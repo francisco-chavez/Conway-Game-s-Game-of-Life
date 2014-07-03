c3d8326f15a55d47bf2f06b42fc1f54a392a1f75
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-02 18:40:04 -0700

    Just putting a few items together

--- ConwayLifeGameSLN/ConwayLifeGame/ViewModels/MainWindowViewModel.cs
+++ ConwayLifeGameSLN/ConwayLifeGame/ViewModels/MainWindowViewModel.cs
@@ -10,5 +10,28 @@ namespace Unv.ConwayLifeGame.ViewModels
 	public class MainWindowViewModel
 		: ViewModel
 	{
+		public virtual CellGridViewModel CellGridViewModel
+		{
+			get { return mn_cellGridViewModel; }
+			protected set
+			{
+				if (mn_cellGridViewModel != value)
+				{
+					mn_cellGridViewModel = value;
+					OnPropertyChanged("CellGridViewModel");
+				}
+			}
+		}
+		private CellGridViewModel mn_cellGridViewModel;
+
+
+		public MainWindowViewModel()
+		{
+			// I know that the "this." isn't needed, but CellGridViewModel = new CellGridViewModel()
+			// can look a bit off when you read the property name without the IDE color coding certain
+			// items for the developer.
+			// -FCT
+			this.CellGridViewModel = new CellGridViewModel();
+		}
 	}
 }
