ecd1f6d565148a2d80688ae031cf28d62d0a9544
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-02 20:58:45 -0700

    Adding Factory for Cells

---
+++ ConwayLifeGameSLN/ConwayLifeGame/Helpers/CellFactory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+using Unv.ConwayLifeGame.ViewModels;
+
+
+namespace Unv.ConwayLifeGame.Helpers
+{
+	public class CellFactory
+	{
+		#region Attributes
+		private DispatcherTimer		m_timer;
+		private CellGridViewModel	m_cellGridViewModel;
+		private List<CellViewModel> m_cells;
+		private Random				m_randomGen;
+		#endregion
+
+
+		#region Constructors
+		public CellFactory(CellGridViewModel cellGridViewModel)
+		{
+			if (cellGridViewModel == null)
+				throw new ArgumentNullException("Cell Grid is missing");
+
+
+			m_cellGridViewModel = cellGridViewModel;
+
+			m_cells		= new List<CellViewModel>(100);
+			m_randomGen = new Random();
+
+			m_timer		= new DispatcherTimer(DispatcherPriority.Input);
+			m_timer.Interval = TimeSpan.FromMilliseconds(50);
+			m_timer.Tick += Timer_Tick;
+		}
+		#endregion
+
+
+		#region Event Handlers
+		void Timer_Tick(object sender, EventArgs e)
+		{
+			int index = m_cells.Count - 1;
+
+			for (int i = 0; i < 10 && index >= 0; i++, index--)
+			{
+				var cell = m_cells[index];
+				m_cells.RemoveAt(index);
+				m_cellGridViewModel.Cells.Add(cell);
+			}
+
+			if (index < 0)
+				m_timer.Stop();
+		}
+		#endregion
+
+
+		#region Methods
+		public void CreateCells()
+		{
+			int width	= m_cellGridViewModel.ColumnCount;
+			int heigth	= m_cellGridViewModel.RowCount;
+
+			m_cells.AddRange(
+				from row in Enumerable.Range(0, heigth)
+				from column in Enumerable.Range(0, width)
+				select new CellViewModel() { Row = row, Column = column, IsLiving = false, WillKeepLiving = false });
+
+			m_cells.Reverse();
+
+			m_timer.Start();
+		}
+		#endregion
+	}
+}
--- ConwayLifeGameSLN/ConwayLifeGame/ViewModels/CellGridViewModel.cs
+++ ConwayLifeGameSLN/ConwayLifeGame/ViewModels/CellGridViewModel.cs
@@ -1,50 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using Unv.ConwayLifeGame.Helpers;
+
 
 namespace Unv.ConwayLifeGame.ViewModels
 {
 	public class CellGridViewModel
 		: ViewModel
 	{
-		#region Properties
-		public int GridWidth
-		{
-			get { return mn_gridWidth; }
-			private set
-			{
-				if (mn_gridWidth != value)
-				{
-					mn_gridWidth = value;
-					OnPropertyChanged("GridWidth");
-				}
-			}
-		}
-		private int mn_gridWidth;
+		#region Attributes
+		private CellFactory m_cellFactory;
+		#endregion
 
-		public int GridHeight
+
+		#region Properties
+		public virtual int RowCount
 		{
-			get { return mn_gridHeight; }
-			private set
+			get { return mn_rowCount; }
+			protected set
 			{
-				if (mn_gridHeight != value)
+				if (mn_rowCount != value)
 				{
-					mn_gridHeight = value;
-					OnPropertyChanged("GridHeight");
+					mn_rowCount = value;
+					OnPropertyChanged("RowCount");
 				}
 			}
 		}
-		private int mn_gridHeight;
+		private int mn_rowCount;
+
+		public virtual int ColumnCount
+		{
+			get { return mn_columnCount; }
+			protected set
+			{
+				if (mn_columnCount != value)
+				{
+					mn_columnCount = value;
+					OnPropertyChanged("ColumnCount");
+				}
+			}
+		}
+		private int mn_columnCount;
+
+		public virtual ObservableCollection<CellViewModel> Cells
+		{
+			get { return mn_cells; }
+			protected set
+			{
+				if (mn_cells != value)
+				{
+					mn_cells = value;
+					OnPropertyChanged("Cells");
+				}
+			}
+		}
+		private ObservableCollection<CellViewModel> mn_cells;
+		#endregion
+
+
+		#region Constructors
+		public CellGridViewModel()
+		{
+			m_cellFactory = new CellFactory(this);
+
+			Cells = new ObservableCollection<CellViewModel>();
+		}
 		#endregion
 
 
 		#region Methods
-		public void SetNewGrid(int gridWidth, int gridHeight)
+		public void SetNewGrid(int columnCount, int rowCount)
 		{
-			GridWidth = gridWidth;
-			GridHeight = gridHeight;
+			ColumnCount = columnCount;
+			RowCount = rowCount;
 		}
 		#endregion
 	}
