dfe6cf907907e1f0160a1f8316918345936dbff6
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-03 17:39:24 -0700

    Started Tying Cell Grid View with Cell Grid View Model

--- ConwayLifeGameSLN/ConwayLifeGame/Views/CellGridView.xaml.cs
+++ ConwayLifeGameSLN/ConwayLifeGame/Views/CellGridView.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Unv.ConwayLifeGame.ViewModels;
+
 
 namespace Unv.ConwayLifeGame.Views
 {
@@ -20,11 +22,89 @@ namespace Unv.ConwayLifeGame.Views
 	/// Interaction logic for CellGridView.xaml
 	/// </summary>
 	public partial class CellGridView 
-		: ItemsControl
+		: UserControl
 	{
+		#region Attributes
+		public static readonly DependencyProperty ViewModelProperty;
+
+		private bool m_firstTimeLoaded = true;
+		#endregion
+
+
+		#region Properties
+		public CellGridViewModel ViewModel
+		{
+			get { return (CellGridViewModel) GetValue(ViewModelProperty); }
+			set { SetValue(ViewModelProperty, value); }
+		}
+		#endregion
+
+
+		#region Constructors
+		static CellGridView()
+		{
+			ViewModelProperty = DependencyProperty.Register(
+				"ViewModel",
+				typeof(CellGridViewModel),
+				typeof(CellGridView),
+				new PropertyMetadata(
+					null, 
+					ViewModel_Changed));
+		}
+
 		public CellGridView()
 		{
 			InitializeComponent();
+
+			Binding b = new Binding();
+			this.SetBinding(ViewModelProperty, b);
+
+			this.Loaded += CellGridView_Loaded;
 		}
+		#endregion
+
+
+		#region Dependency Property Event Handlers
+		private static void ViewModel_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var me = (CellGridView) d;
+			CellGridViewModel grid;
+			
+			grid = e.OldValue as CellGridViewModel;
+			if (grid != null)
+				grid.GridSizeUpdated -= me.UpdateGridSize;
+
+			grid = e.NewValue as CellGridViewModel;
+			if (grid != null)
+				grid.GridSizeUpdated += me.UpdateGridSize;
+
+			me.UpdateGridSize(null, null);
+		}
+		#endregion
+
+
+		#region Event Handlers
+		void CellGridView_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (!m_firstTimeLoaded)
+				return;
+
+			ViewModel.SetNewGrid(10, 10);
+		}
+
+		private void UpdateGridSize(object source, EventArgs e)
+		{
+			PART_Grid.ColumnDefinitions.Clear();
+			PART_Grid.RowDefinitions.Clear();
+
+			if (ViewModel == null)
+				return;
+
+			for (int i = 0; i < ViewModel.ColumnCount; i++)
+				PART_Grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Star) });
+			for (int i = 0; i < ViewModel.RowCount; i++)
+				PART_Grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1.0, GridUnitType.Star) });
+		}
+		#endregion
 	}
 }
