372621bdbc83ef22a2cc187387b6b6b2f2bbe7e9
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-03 18:35:51 -0700

    Added Column and Row Count Properties to Cell Grid Panel
    I need to be able to change the number of rows and columns of the grid
    at run time. This isn't much of an issue, but I also need a way to set
    the cells into the grid. The easiest way I can think of this is to set
    the grid as the panel of an items control (letting me bind the items
    source to my cell collection). Once I do that, I will loose easy access
    to the grid. I should be able to get access to it (somehow) in code, but
    I'm going to try setting the column and row counts in a parent first and
    see if that works.

--- ConwayLifeGameSLN/ConwayLifeGame/Controls/CellGridPanel.cs
+++ ConwayLifeGameSLN/ConwayLifeGame/Controls/CellGridPanel.cs
@@ -14,5 +14,99 @@ namespace Unv.ConwayLifeGame.Controls
 	public class CellGridPanel
 		: Grid
 	{
+		#region Attributes
+		public static readonly DependencyProperty ColumnCountProperty;
+		public static readonly DependencyProperty RowCountProperty;
+		#endregion
+
+
+		#region Properties
+		protected int ColumnCount
+		{
+			get { return (int) GetValue(ColumnCountProperty); }
+		}
+		protected int RowCount
+		{
+			get { return (int) GetValue(RowCountProperty); }
+		}
+		#endregion
+
+
+		#region Constructors
+		static CellGridPanel()
+		{
+			ColumnCountProperty = DependencyProperty.Register(
+				"ColumnCount",
+				typeof(int),
+				typeof(CellGridPanel),
+				new FrameworkPropertyMetadata(
+					3,
+					FrameworkPropertyMetadataOptions.Inherits,
+					ColumnCount_Changed));
+
+			RowCountProperty = DependencyProperty.Register(
+				"RowCount",
+				typeof(int),
+				typeof(CellGridPanel),
+				new FrameworkPropertyMetadata(
+					3,
+					FrameworkPropertyMetadataOptions.Inherits,
+					RowCount_Changed));
+		}
+		#endregion
+
+
+		#region Depdendency Property Event Handlers
+		private static void ColumnCount_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (!(d is CellGridPanel))
+				return;
+
+			CellGridPanel obj = (CellGridPanel) d;
+			int columnCount = (int) e.NewValue;
+
+			obj.ColumnDefinitions.Clear();
+
+			for (int i = 0; i < columnCount; i++)
+				obj.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Star) });
+		}
+
+		private static void RowCount_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (!(d is CellGridPanel))
+				return;
+
+			CellGridPanel obj = (CellGridPanel) d;
+			int rowCount = (int) e.NewValue;
+
+			obj.RowDefinitions.Clear();
+
+			for (int i = 0; i < rowCount; i++)
+				obj.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1.0, GridUnitType.Star) });
+		}
+		#endregion
+
+
+		#region Methods
+		public static void SetColumnCount(UIElement element, int value)
+		{
+			element.SetValue(ColumnCountProperty, value);
+		}
+
+		public static int GetColumnCount(UIElement element)
+		{
+			return (int) element.GetValue(ColumnCountProperty);
+		}
+
+		public static void SetRowCount(UIElement element, int value)
+		{
+			element.SetValue(RowCountProperty, value);
+		}
+
+		public static int GetRowCount(UIElement element)
+		{
+			return (int) element.GetValue(RowCountProperty);
+		}
+		#endregion
 	}
 }
