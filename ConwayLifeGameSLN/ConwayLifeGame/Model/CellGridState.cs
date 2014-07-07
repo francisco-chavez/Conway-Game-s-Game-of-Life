98df48c4587ca95c0498e3daddc2ac979b450597
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-06 19:54:02 -0700

    Adding Items that will be used for controlling the game's progress

---
+++ ConwayLifeGameSLN/ConwayLifeGame/Model/CellGridState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unv.ConwayLifeGame.Model
+{
+	public enum CellGridState
+	{
+		SettingInitialGeneration	= 0,
+		ManualProgression			= 1,
+		AutoProgression				= 2
+	}
+}
