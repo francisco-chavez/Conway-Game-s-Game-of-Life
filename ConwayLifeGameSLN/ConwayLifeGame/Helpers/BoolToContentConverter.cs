dfcfb4d128337b11ebd99e7da69bcb54f060f3c0
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-10 19:36:24 -0700

    Adding a Bool To Convent Converter class.
    The Bool to Content Converter class is something I came up with when I
    was generalizing a Bool to Solid Color Brush Converter class I wrote for
    a different project a few years ago.  For this project, the converter
    will be used to change the content in the button that starts/stops the
    auto progression in the game.

---
+++ ConwayLifeGameSLN/ConwayLifeGame/Helpers/BoolToContentConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace Unv.ConwayLifeGame.Helpers
+{
+	public class BoolToContentConverter
+		: IValueConverter
+	{
+		#region Properties
+		/// <summary>
+		/// Gets or sets the content object to return when
+		/// the given value to convert is True.
+		/// </summary>
+		public object TrueContent	{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the content object to return when
+		/// the given value to convert is False.
+		/// </summary>
+		public object FalseContent	{ get; set; }
+
+		/// <summary>
+		/// Gets or sets the content object to return when
+		/// the given value to convert is either Null or
+		/// unable to be converted to a boolean value.
+		/// </summary>
+		public object NullContent	{ get; set; }
+		#endregion
+
+
+		#region Constructors
+		public BoolToContentConverter()
+		{
+			this.TrueContent	= "True";
+			this.FalseContent	= "False";
+			this.NullContent	= "Null";
+		}
+		#endregion
+
+
+		#region Methods
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var boolean = value as bool?;
+
+			return 
+				boolean.HasValue 
+				? (boolean.Value ? TrueContent : FalseContent) 
+				: NullContent;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
+}
