033a3a460a05a424b0b838db1756674509721f73
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-14 19:00:28 -0700

    Working on Game Progression
    Finished implementing the class that will progress the cells' living
    states each turn.

--- ConwayLifeGameSLN/ConwayLifeGame/Helpers/ConwayCellProgressor.cs
+++ ConwayLifeGameSLN/ConwayLifeGame/Helpers/ConwayCellProgressor.cs
@@ -15,6 +15,22 @@ namespace Unv.ConwayLifeGame.Helpers
 		{
 			for (int i = 0; i < cells.Length; i++)
 			{
+				int livingNeighborCount = GetLivingNeighborCount(cells, columns, rows, i);
+
+				switch (livingNeighborCount)
+				{
+				case 2:
+					cells[i].WillKeepLiving = cells[i].IsLiving;
+					break;
+
+				case 3:
+					cells[i].WillKeepLiving = true;
+					break;
+
+				default:
+					cells[i].WillKeepLiving = false;
+					break;
+				}
 			}
 
 			for (int i = 0; i < cells.Length; i++)
@@ -25,6 +41,63 @@ namespace Unv.ConwayLifeGame.Helpers
 		{
 			int count = 0;
 
+			// Find our current location in the grid
+			int currentRow		= currentIndex / columns;
+			int currentColumn	= currentIndex - (currentRow * columns);
+
+			// Find out which directions from our current position
+			// have neighbors
+			bool hasNeighborsAbove		= currentRow != 0;
+			bool hasNeighborsBelow		= currentRow != rows - 1;
+			bool hasNeighborsOnLeft		= currentColumn != 0;
+			bool hasNeighborsOnRight	= currentColumn != columns - 1;
+
+			// Find the indeces of the neighbors
+			int topIndex		= currentIndex - columns;
+			int topLeftIndex	= topIndex - 1;
+			int topRightIndex	= topIndex + 1;
+
+			int leftIndex		= currentIndex - 1;
+			int rightIndex		= currentIndex + 1;
+
+			int bottomIndex		= currentIndex + columns;
+			int bottomLeftIndex = bottomIndex - 1;
+			int bottomRightIndex = bottomIndex + 1;
+
+			// Check row above for living neighbors
+			if (hasNeighborsAbove)
+			{
+				if (hasNeighborsOnLeft && cells[topLeftIndex].IsLiving)
+					count++;
+
+				if (cells[topIndex].IsLiving)
+					count++;
+
+				if (hasNeighborsOnRight && cells[topRightIndex].IsLiving)
+					count++;
+			}
+
+			// Check current row for living neighbors
+			if (hasNeighborsOnLeft && cells[leftIndex].IsLiving)
+				count++;
+
+			if (hasNeighborsOnRight && cells[rightIndex].IsLiving)
+				count++;
+
+			// Check row below for living neighbors
+			if (hasNeighborsBelow)
+			{
+				if (hasNeighborsOnLeft && cells[bottomLeftIndex].IsLiving)
+					count++;
+
+				if (cells[bottomIndex].IsLiving)
+					count++;
+
+				if (hasNeighborsOnRight && cells[bottomRightIndex].IsLiving)
+					count++;
+			}
+
+			// Return count of living neighbors
 			return count;
 		}
 	}
