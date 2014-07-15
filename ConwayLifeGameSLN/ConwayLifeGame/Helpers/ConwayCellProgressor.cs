1ec46cd8399eb37a38960ca7eeea63d04765f3ab
francisco-chavez <francisco.chavez.509@outlook.com>
2014-07-14 18:35:04 -0700

    Working on Game Progression
    I've added a class that will update the cells to their next living
    state.

---
+++ ConwayLifeGameSLN/ConwayLifeGame/Helpers/ConwayCellProgressor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Unv.ConwayLifeGame.ViewModels;
+
+
+namespace Unv.ConwayLifeGame.Helpers
+{
+	public class ConwayCellProgressor
+	{
+		public virtual void StepCells(CellViewModel[] cells, int columns, int rows)
+		{
+			for (int i = 0; i < cells.Length; i++)
+			{
+			}
+
+			for (int i = 0; i < cells.Length; i++)
+				cells[i].IsLiving = cells[i].WillKeepLiving;
+		}
+
+		protected virtual int GetLivingNeighborCount(CellViewModel[] cells, int columns, int rows, int currentIndex)
+		{
+			int count = 0;
+
+			return count;
+		}
+	}
+}
